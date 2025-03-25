c7ece5715b95ca4359d277bf71cc635ead77fdb3
amit-amsl <202411926+amit-amsl@users.noreply.github.com>
2025-03-26 00:01:56 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from 'express';
+import { createServer } from 'http';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const ORIGIN_ADDR = 'http://localhost:5173';
+
+const app = express();
+const httpServer = createServer(app);
+
+app.set('trust proxy', true);
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(
+  cors({
+    origin: ORIGIN_ADDR,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+httpServer.listen(PORT, () => console.log(`listening on port ${PORT}!`));
